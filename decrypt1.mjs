db5112284edf541d4646b1cbf0995bc52e01ff5b
wildanvin <danvin.jim@gmail.com>
2023-02-21 17:05:40 -0500

    Update decrypt1

--- decrypt1.mjs
+++ decrypt1.mjs
@@ -3,7 +3,6 @@ dotenv.config()
 
 import { Medusa } from '@medusa-network/medusa-sdk'
 import { ethers, BigNumber } from 'ethers'
-import { abi, applicationAddress } from './constants.mjs'
 
 // Hyperspace Testnet Medusa Address = "0xd466a3c66ad402aa296ab7544bce90bbe298f6a0";
 // Arbitrum Testnet Medusa Address = "0xf1d5A4481F44fe0818b6E7Ef4A60c0c9b29E3118";
@@ -26,21 +25,21 @@ const medusa = await Medusa.init(medusaAddress, signer)
 const ciphertext = {
   random: {
     x: BigNumber.from(
-      '0x34DBF0FB80BC4216686ECB8C46B8D0F1B6D45EECDCA387F579C4AEF20B9C78E'
+      '0x2ce01487a632325a27fc1c3056acf3ed03697f3845ebfa94a92614937b43139a'
     ),
     y: BigNumber.from(
-      '0x1C986273FEB639A2097DC79C90FBB1BBD2B1508B88FD67A1F6DD68E8EDE81557'
+      '0x286d9508e0b88a2a9a6a806a8d6aee3a3a69134aec30b3ec11476bec39d25256'
     ),
   },
   cipher: BigNumber.from(
-    '0x65FBD46AE46BF33C13C9EA4DBD49EBED6FCA7E2B6C3FDCA3B9157AE884EF48D'
+    '0x909811a4516fb709923a44ac1fa1d15ba24446716384d4ac3d459bf9e248973a'
   ),
   random2: {
     x: BigNumber.from(
-      '0x34DBF0FB80BC4216686ECB8C46B8D0F1B6D45EECDCA387F579C4AEF20B9C78E'
+      '0x2ce01487a632325a27fc1c3056acf3ed03697f3845ebfa94a92614937b43139a'
     ),
     y: BigNumber.from(
-      '0x1C986273FEB639A2097DC79C90FBB1BBD2B1508B88FD67A1F6DD68E8EDE81557'
+      '0x286d9508e0b88a2a9a6a806a8d6aee3a3a69134aec30b3ec11476bec39d25256'
     ),
   },
   dleq: {
@@ -50,10 +49,10 @@ const ciphertext = {
 }
 
 const blob = new Uint8Array([
-  6, 81, 11, 142, 60, 159, 173, 90, 10, 43, 68, 131, 65, 38, 73, 141, 8, 238,
-  93, 116, 208, 15, 251, 198, 19, 95, 249, 224, 141, 198, 8, 24, 108, 56, 13,
-  243, 180, 41, 8, 226, 43, 5, 77, 196, 161, 81, 66, 218, 15, 148, 248, 175,
-  180, 47, 123, 171, 130, 124, 6, 61, 95, 95,
+  161, 54, 97, 85, 81, 117, 150, 122, 157, 125, 81, 46, 112, 188, 178, 224, 207,
+  99, 149, 239, 122, 85, 170, 235, 21, 3, 151, 101, 63, 41, 24, 98, 29, 197, 21,
+  215, 150, 140, 43, 237, 215, 156, 143, 63, 226, 152, 121, 252, 116, 64, 145,
+  160, 73, 40, 40, 177, 180, 212, 123, 204, 213, 84, 198, 141, 207,
 ])
 
 const decryptedBytes = await medusa.decrypt(ciphertext, blob)
